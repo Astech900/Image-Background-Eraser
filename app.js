fe940a2cf1eb9d4db571539b3483f29dd737bcfd
AsTech-900 <sallu@gmail.com>
2025-02-15 23:53:46 +0600

    added new features

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ let imgBox = document.querySelector('.img-box');
 let imgWrap = document.querySelector('.img-wrap');
 let originalImg = document.getElementById("originals");
 let line = document.getElementById('line');
+let changeImgBtn = document.getElementById("change-photo");
+let transImg= document.getElementById("trans");
+
+changeImgBtn.addEventListener("click",()=>{
+  if(transImg.src.includes("background_change_img/transparent.png") || originalImg.src.includes("background_change_img/original.jpg") ){
+    transImg.src = "background_change_img/benz-tran.png";
+     originalImg.src = "background_change_img/benz-original.jpg"
+     
+    
+  }else{
+    transImg.src = "background_change_img/transparent.png";
+    originalImg.src = "background_change_img/original.jpg";
+   
+    
+  }
+   
+    
+})
 
 originalImg.style.width = imgBox.offsetWidth + "px";
 let leftSpace = imgBox.offsetLeft;
